20d71c622d68ba54f8ee85712f9b656c13f60fc2
EPX-PANCA <rectapanca@gmail.com>
2020-07-27 22:42:24 +0700

    up

---
+++ 08.js
@@ -0,0 +1,22 @@
+const {program} = require('@caporal/core');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const {logger} = require('caporal');
+
+program
+    .command('headlines', 'get title link')
+    .action(() => {
+        let data = [];
+        const scrape = async () => {
+            const html = await axios.get('https://kompas.com');
+            const $ = await cheerio.load(html.data);
+
+            $('.article__link').each((i, element) => {
+                data.push({title: $(element).text(), link: $(element).attr('href')})
+            });
+            console.log(data);
+        }
+        scrape()
+    })
+
+program.run()
