413412fb2b95e341667229d822ce2b749ff8d684
EPX-PANCA <rectapanca@gmail.com>
2020-07-28 22:24:02 +0700

    up

---
+++ 03.js
@@ -0,0 +1,9 @@
+const output = async () => {
+    for (let i = 1; i <= 3; i++) {
+        await new Promise(resolve => 
+            setTimeout(resolve, 1000))
+        console.log(`${i},`);
+    }
+    console.log('Done');
+}
+output()
