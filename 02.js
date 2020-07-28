08a64716d69edbdb972780dcdb5431b484ca5865
EPX-PANCA <rectapanca@gmail.com>
2020-07-28 23:15:59 +0700

    up

---
+++ 02.js
@@ -0,0 +1,39 @@
+const axios = require('axios')
+
+const urlUser = "http://jsonplaceholder.typicode.com/users";
+const urlPost = "http://jsonplaceholder.typicode.com/posts";
+
+
+axios.get(`${urlUser}`)
+.then( response => {
+  const result = response.data
+  console.log(result);
+})
+.catch(error => {
+  console.log(error);
+})
+
+  
+
+ axios.get(`${urlPost}`)
+  .then( response => {
+    const result = response.data
+    console.log(result);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+async () => {
+  try {
+    const responseUser = await get(`${urlUser}`);
+   console.log(responseUser);
+    const responsePost = await axios.get(`${urlPost}`);
+   console.log(responsePost);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
