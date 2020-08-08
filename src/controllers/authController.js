d951c47e66d0becb591940f589a08f1d23adb2b9
EPX-PANCA <rectapanca@gmail.com>
2020-08-08 17:38:51 +0700

    up skuy

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -53,33 +53,33 @@ res.json("Data ga cocok, coba cek lagi biar bisa generate token")
 
 //ini buat tes get user aja
 
-    static async user(req, res) {
+//     static async user(req, res) {
 
-        passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    models.user.findOne({
-        where: {
-            username: jwt_payload
-        }
-    })
-    .then((data) => {
-        const user = data;
-        return done(null, user)
-    })
-    .catch((err) => { return done(err, false) })
-}));
+//         passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+//     models.user.findOne({
+//         where: {
+//             username: jwt_payload
+//         }
+//     })
+//     .then((data) => {
+//         const user = data;
+//         return done(null, user)
+//     })
+//     .catch((err) => { return done(err, false) })
+// }));
 
-passport.serializeUser(function(user, cb) {
-    cb(null, user.id);
-});
+// passport.serializeUser(function(user, cb) {
+//     cb(null, user.id);
+// });
   
-passport.deserializeUser(function(id, cb) {
-    models.user.findByPk(id, function (err, user) {
-        if (err) { return cb(err); }
-        cb(null, user);
-    });
-});
-        res.json("hallo")
-    }
+// passport.deserializeUser(function(id, cb) {
+//     models.user.findByPk(id, function (err, user) {
+//         if (err) { return cb(err); }
+//         cb(null, user);
+//     });
+// });
+//         res.json("hallo")
+//     }
 
 
 
--- src/routes/auths.js
+++ src/routes/auths.js
@@ -1,8 +1,9 @@
 const authController = require("../controllers/authController")
 const router = require('express').Router();
-const passport = require("passport");
+//const middle = require("../middleware/middle");
+
 
 router.post('/login', authController.login)
-router.get('/user', passport.authenticate("jwt",{session: false}), authController.user)
+//router.get('/user', middle, authController.user)
 
 module.exports = router
