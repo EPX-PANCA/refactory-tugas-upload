fe2e2f4673da464655777163edec70e2e5219325
EPX-PANCA <rectapanca@gmail.com>
2020-09-02 17:48:41 +0700

    init

--- 2.py
+++
@@ -1,9 +0,0 @@
-import operator
-def dua():
-    menus = ["chicken strip", "beef burger", "steakhouse", "mushroom swiss", "whopper"]
-    price = [15,10,12,20,30]
-    merge = dict(zip(menus, price))
-    sort = sorted(merge.items(), key=operator.itemgetter(1))
-    print(dict(sort))
-
-dua()
