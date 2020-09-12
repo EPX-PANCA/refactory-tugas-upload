015a70a405231c8e4d608c3001b48ed1447b5d3d
EPX-PANCA <rectapanca@gmail.com>
2020-09-12 10:59:30 +0700

    done

--- urls.sh
+++ urls.sh
@@ -6,7 +6,7 @@ while read -r p; do
     name=${p##*/}
     touch "${name}.json"
     echo $data > "${name}.json"
-    echo "${data}"
+    echo "${name}.json created"
 done < $urltxt
 
 #udh bisa, tapi ngetestnya di repl.it
